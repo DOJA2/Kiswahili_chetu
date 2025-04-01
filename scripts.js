49d4fe7c1f3f960cf8e2fb13bac1f82795dabc6d
DOJA2 <kamandarogati@gmail.com>
2025-04-01 09:44:59 +0300

    remove some files

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript code to add interactivity if needed
-    console.log("DOM fully loaded and parsed");
-});
-
-function toggleMenu() {
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('show');
-  }
